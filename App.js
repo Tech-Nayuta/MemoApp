81e8271d78ad86b955184225da31d1cad78502f2
Tech-Nayuta <ketanayutawork@gmail.com>
2020-10-02 15:56:53 +0900

    tsuiniugoita

--- App.js
+++ App.js
@@ -8,15 +8,23 @@ import MemoDetailScreen from './src/screens/MemoDetailScreen';
 import MemoEditScreen from './src/screens/MemoEditScreen';
 import LoginScreen from './src/screens/LoginScreen';
 import SignupScreen from './src/screens/SignupScreen';
-
-
+ 
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
  
+// 3.x 系では、
+// import { createAppContainer, createStackNavigator } from 'react-navigation';
+// のようにしていましたが、パッケージが分離されたため、それぞれ個別で読み込む必要があります。
+ 
 const App = createStackNavigator({
   Home: { screen: MemoListScreen },
+}, {
+  defaultNavigationOptions: {
+    // ... オプション
+  }
 });
  
+export default createAppContainer(App);
 
 // // export default createAppContainer(App);
 // export default function App() {
@@ -42,4 +50,5 @@ const App = createStackNavigator({
 //   },
 // });
 
-export default App;
+// export default App;
+// export default createAppContainer(App);
