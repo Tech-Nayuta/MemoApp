ac7c3c9f04fce4e4249ae891ba43470619617a47
Tech-Nayuta <ketanayutawork@gmail.com>
2020-10-04 17:58:08 +0900

    Hide firebase certificate

--- App.js
+++ App.js
@@ -11,18 +11,36 @@ import SignupScreen from './src/screens/SignupScreen';
  
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
- 
- 
+import firebase from 'firebase';
+
+import ENV from './env.json'
+
+const firebaseConfig = {
+  apiKey: ENV.FIREBASE_API_KEY,
+  authDomain: ENV.FIREBASE_AUTH_DOMAIN,
+  databaseURL: ENV.FIREBASE_DB_URL,
+  projectId: ENV.FIREBASE_PRJ_ID,
+  storageBucket:  ENV.FIREBASE_STORAGE,
+  messagingSenderId: ENV.FIREBASE_SENDER_ID,
+  appId: ENV.FIREBASE_APP_ID,
+  measurementId: ENV.FIREBASE_MEASURE_ID,
+};
+
+if(firebase.apps.length == 0){ //initializeAppが複数回呼ばれるとエラーに
+  firebase.initializeApp(firebaseConfig);
+}
+
 const App = createStackNavigator({
+  Login:      { screen: LoginScreen },
+  Signup:     { screen: SignupScreen },
   Home:       { screen: MemoListScreen },
   MemoDetail: { screen: MemoDetailScreen },
   MemoEdit:   { screen: MemoEditScreen },
-  Login:      { screen: LoginScreen },
-  Signup:     { screen: SignupScreen },
-
 }, {
   defaultNavigationOptions: {
+    headerBackTitle: " ",
     headerTitle: 'Momot',
+    headerTintColor: '#fff',
     headerTitleStyle:{
       color: '#fff',
     },
